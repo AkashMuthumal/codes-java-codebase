e3b998206061c2ca88a384d36d27551d8555b50c
AkashMuthumal <66267057+AkashMuthumal@users.noreply.github.com>
2023-09-26 14:22:37 +0000

    first commit

---
+++ stackTest.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+/**
+ * stackTest
+ */
+public class stackTest {
+    public static void main(String[] args) {
+        Stack<String> myStack = new Stack<String>();
+
+        myStack.push("first element");
+        myStack.push("second element");
+
+        System.out.println(myStack);
+    }
+    
+}
